baa43446cb77f7690b77be1fa6668ded45869843
ilivanilton <ilivanilton@gmail.com>
2018-11-09 04:31:39 -0300

    melhorias no game

--- forca.py
+++ forca.py
@@ -4,12 +4,23 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_","_","_","_","_","_"]
 
     enforcou = False
     acertou = False
 
     while not acertou and not enforcou:
-        for 
+        chute = input("Qual letra?").lower().strip()
+
+        idx = 0
+        for letra in palavra_secreta:
+            if letra == chute:
+                print("Acertou a letra {} na posição {}".format(chute, idx))
+                letras_acertadas[idx] = chute
+            idx = idx + 1
+        print("====================")
+        print(letras_acertadas)
+        print("====================")
         print("Jogando...")
 
     print("Fim do jogo")

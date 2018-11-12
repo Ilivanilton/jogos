6a095c0702eb1de2a1d474d958ea49ccd8046b47
ilivanilton <ilivanilton@gmail.com>
2018-11-12 14:49:11 -0300

    implementado logica de contagem

--- forca.py
+++ forca.py
@@ -3,26 +3,35 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_","_","_","_","_","_"]
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    #for letra in palavra_secreta:
+     #   letras_acertadas.append("_")
 
     enforcou = False
     acertou = False
+    erros = 0
 
     while not acertou and not enforcou:
-        chute = input("Qual letra?").lower().strip()
 
-        idx = 0
-        for letra in palavra_secreta:
-            if letra == chute:
-                print("Acertou a letra {} na posição {}".format(chute, idx))
-                letras_acertadas[idx] = chute
-            idx = idx + 1
-        print("====================")
-        print(letras_acertadas)
-        print("====================")
-        print("Jogando...")
+        chute = input("Qual letra?").upper().strip()
 
+        if(chute in palavra_secreta):
+            idx = 0
+            for letra in palavra_secreta:
+                if letra == chute:
+                    print("Acertou a letra {} na posição {}".format(chute, idx))
+                    letras_acertadas[idx] = chute
+                idx += 1
+            print("====================")
+            print(letras_acertadas)
+            print("====================")
+            print("Jogando...")
+        else:
+            erros += 1
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
     print("Fim do jogo")
 
 
